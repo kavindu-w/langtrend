c527dfe1fd3e9dec64151c967453c6c2b9e66230
kavindu-w <akwarnakulasuriya@gmail.com>
2026-05-29 02:25:58 +0800

    switch back to github pages with static website

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
-  site: 'https://langtrend.vercel.app',
+  site: 'https://kavindu-w.github.io',
+  base: '/langtrend/',
 });
